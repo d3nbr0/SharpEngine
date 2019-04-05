977e470f9589df73ccc3709744774e5891936d27
DenBroShow <denchik.ktm@gmail.com>
2019-04-05 23:58:00 +0300

    Add: Multinames for bot commands

--- bot/command.py
+++ bot/command.py
@@ -22,6 +22,8 @@ def load_commands():
             if hasattr(cmd, 'cmd'):
                 if 'disabled' not in cmd.cmd or not cmd.cmd['disabled']:
                     cmd.error = bot.module.catch
+                    if not isinstance(cmd.cmd['name'], list):
+                        cmd.cmd['name'] = [cmd.cmd['name']]
                     commands.append(cmd)
             else:
                 console.error("Команда \"{}\\{}\" не была загружена".format(root, item))
@@ -30,19 +32,20 @@ def load_commands():
 def exec_command(message, rmsg):
     args = message['text'].lower().split(' ')
     for item in commands:
-        if item.cmd['name'] == args[0]:
-            user = bot.module.user.User(message['from_id'])
-            try:
-                item.cmd['processing']({'msg': message, 'args': args, 'other': {'retime': time.time()}}, user, rmsg)
-            except bot.module.CommandException as cmd:
-                rmsg.add_line(cmd.message)
-            except Exception:
-                ex_type, ex, tb = sys.exc_info()
-                rmsg.reset()
-                rmsg.add_line('❌ Возникла системная ошибка при обработке команды')
-                console.error("Ошибка при выполнении команды \"{}\": ({}) {}\n{}".format(message['text'], ex_type, ex, traceback.format_tb(tb)))
-            if rmsg.nickname():
-                rmsg.add_start_line("{}, ".format(user.nickname))
-            user.save()
-            return True
+        for cmd_name in item.cmd['name']:
+            if cmd_name == args[0]:
+                user = bot.module.user.User(message['from_id'])
+                try:
+                    item.cmd['processing']({'msg': message, 'args': args, 'other': {'retime': time.time()}}, user, rmsg)
+                except bot.module.CommandException as cmd:
+                    rmsg.add_line(cmd.message)
+                except Exception:
+                    ex_type, ex, tb = sys.exc_info()
+                    rmsg.reset()
+                    rmsg.add_line('❌ Возникла системная ошибка при обработке команды')
+                    console.error("Ошибка при выполнении команды \"{}\": ({}) {}\n{}".format(message['text'], ex_type, ex, traceback.format_tb(tb)))
+                if rmsg.nickname():
+                    rmsg.add_start_line("{}, ".format(user.nickname))
+                user.save()
+                return True
     return False
